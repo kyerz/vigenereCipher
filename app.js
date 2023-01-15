694af533b652b333f2a64969c60f7b515f309819
kyerz <mikael.verdu@gmail.com>
2023-01-15 16:52:48 +0100

    improve css

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ inputAlphabet.value = dictionary;
 
 const inputKey = document.querySelector("#encryptKey");
 inputKey.value = key;
+
+const msgToEncrypt = document.querySelector("#clearMsg");
+
+const cipherBtn = document.querySelector(".cipher");
+
+cipherBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("chiffrement en cours...", msgToEncrypt.value);
+});
